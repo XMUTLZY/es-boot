368bccd3ec75ce486aede8f99b2ccb59f8a2e493
Jake.lin <jake.lin@qeeka.com>
2020-09-16 15:15:20 +0800

    es boot

---
+++ src/test/java/jake/esboot/EsBootApplicationTests.java
@@ -0,0 +1,13 @@
+package jake.esboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
